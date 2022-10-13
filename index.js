ddc49e22f0794c6d7c5bb8b39e5914c7197c6503
Yousif A. Dhahir <Yusuf.it.01@gmail.com>
2022-10-13 15:24:28 +0300

    Completed assignment v2

--- index.js
+++ index.js
@@ -34,34 +34,3 @@ document.addEventListener("keydown", function (e) {
       dodger.style.left = `${left + 1}px`;
     }
   }
-
-
-// function moveDodgerLeft() {
-//   const leftNumbers = dodger.style.left.replace("px", "");
-//   const left = parseInt(leftNumbers, 10);
-
-//   if (left > 0) {
-//     dodger.style.left = `${left - 1}px`;
-//   }
-// }
-
-// document.addEventListener("keydown", function (e) {
-//   if (e.key === "ArrowLeft") {
-//     moveDodgerLeft();
-//   }
-// });
-
-// function moveDodgerRight() {
-//   const rightNumbers = dodger.style.left.replace("px", "");
-//   const left = parseInt(rightNumbers, 10);
-
-//   if (left < 360) {
-//     dodger.style.left = `${left + 1}px`;
-//   }
-// }
-
-// document.addEventListener("keydown", function (e) {
-//   if (e.key === "ArrowRight") {
-//     moveDodgerRight();
-//   }
-// });
